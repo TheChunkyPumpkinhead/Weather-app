95be41e87a1f2d120c3f9d6282cc6e5486c07bcd
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2025-03-09 18:16:47 -0700

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Weather from "./components/Weather";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
 };
 
 export default App;
