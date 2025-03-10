da05498c1e57caa07b407e4f1f04a9e6d54a87c4
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2025-03-10 09:44:15 -0700

    added icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -17,7 +17,25 @@ const Weather = () => {
         <img src={search_icon} alt="" />
       </div>
       <img src={clear_icon} alt="" className="weather-icon" />
-      <p>16</p>
+      <p className="temperature">16°c</p>
+      <p className="location">London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.6 Mph</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
